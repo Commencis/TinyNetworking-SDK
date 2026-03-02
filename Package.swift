2c369639a081f7da029595f634de185bd9a6fb7c
Ekrem Duvarbasi <ekrem.duvarbasi@commencis.com>
2026-03-02 13:01:40 +0300

    Update 2.1.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.10
+// swift-tools-version:6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,9 +9,15 @@ let package = Package(
     products: [
         .library(
             name: "TinyNetworking",
-            targets: ["TinyNetworking"]),
+            targets: ["TinyNetworking"]
+        ),
+        .library(
+            name: "TinyNetworkingOpenAPI",
+            targets: ["TinyNetworkingOpenAPI"]
+        )
     ],
     targets: [
-        .binaryTarget(name: "TinyNetworking", path: "artifacts/TinyNetworking.xcframework")
+        .binaryTarget(name: "TinyNetworking", path: "artifacts/TinyNetworking.xcframework"),
+        .binaryTarget(name: "TinyNetworkingOpenAPI", path: "artifacts/TinyNetworkingOpenAPI.xcframework")
     ]
 )
